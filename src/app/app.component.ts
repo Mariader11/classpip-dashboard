2c7c445910920fe3e760f600357c2ee0d5632a07
Vila Conesa, Ferran <Ferran.Vila@gft.com>
2016-09-24 17:45:56 +0200

    Upgrade angular version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,10 @@
 import { Component } from '@angular/core';
-import { SamplePipe, SampleComponent, SampleService } from 'classpip-utils';
 
 @Component({
-  moduleId: module.id,
   selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css'],
-  pipes : [SamplePipe],
-  directives: [SampleComponent],
-  providers: [SampleService]
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
   title = 'app works!';
-  constructor(sampleService: SampleService) {
-    console.log('AppComponent init!');
-    console.log(sampleService.getElements());
-  }
 }
