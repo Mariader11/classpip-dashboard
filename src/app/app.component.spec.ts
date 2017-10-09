ed4e91e2237a04de2bd14ca7f65e15f19b6282b3
Ferran Vila Conesa <ikocel1@gmail.com>
2017-10-09 07:24:25 +0200

    angular 4 migration

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,87 +1,27 @@
-/* tslint:disable:no-unused-variable */
-
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { APP_BASE_HREF } from '@angular/common';
 import { TestBed, async } from '@angular/core/testing';
-import { FormsModule } from '@angular/forms';
-import { Http, HttpModule } from '@angular/http';
-import { TranslateModule, TranslateLoader, TranslateStaticLoader } from 'ng2-translate/ng2-translate';
-
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
-// aplication
 import { AppComponent } from './app.component';
-import { AppConfig } from './app.config';
-import { routing } from './app.routing';
-
-// pages
-import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
-import { StudentsComponent } from './students/students.component';
-
-// components
-import { AlertComponent } from './_directives/index';
-import { OrderByIdPipe, OrderByNamePipe, OrderBySurnamePipe } from './_pipes/index';
-import { AuthGuard } from './_guards/index';
-import {
-  AlertService, AngularService, AvatarService, GradeService, GroupService,
-  LoginService, MatterService, SchoolService, UserService, UtilsService
-} from './_services/index';
-
-export function createTranslateLoader(http: Http) {
-  return new TranslateStaticLoader(http, AppConfig.LANG_PATH, AppConfig.LANG_EXT);
-}
-
 describe('AppComponent', () => {
-
-  beforeEach(() => {
+  beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent,
-        AlertComponent,
-        LoginComponent,
-        HomeComponent,
-        StudentsComponent,
-        // pipes
-        OrderByIdPipe,
-        OrderByNamePipe,
-        OrderBySurnamePipe
+        AppComponent
       ],
-      imports: [
-        NgbModule.forRoot(),
-        BrowserModule,
-        FormsModule,
-        HttpModule,
-        routing,
-        TranslateModule.forRoot({
-          provide: TranslateLoader,
-          useFactory: createTranslateLoader,
-          deps: [Http]
-        })
-      ],
-      providers: [
-        { provide: APP_BASE_HREF, useValue: '/' },
-        AuthGuard,
-        AlertService,
-        AngularService,
-        AvatarService,
-        GradeService,
-        GroupService,
-        LoginService,
-        MatterService,
-        SchoolService,
-        UserService,
-        UtilsService
-      ],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA]
-    });
-    TestBed.compileComponents();
-  });
-
+    }).compileComponents();
+  }));
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
+  }));
+  it(`should have as title 'app'`, async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('app');
+  }));
+  it('should render title in a h1 tag', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
   }));
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { TranslateService } from 'ng2-translate';
 
 import { AppConfig } from './app.config';
-import { AngularService, AlertService, UserService, UtilsService } from './_services/index';
-import { Profile, Login } from './_models/index';
+//import { AngularService, AlertService, UserService, UtilsService } from './_services/index';
+//import { Profile, Login } from './_models/index';
 
 @Component({
   selector: 'app-root',
@@ -12,14 +12,14 @@ import { Profile, Login } from './_models/index';
 })
 export class AppComponent implements OnInit {
 
-  public menu: boolean;
-  public profile: Profile;
+  //public menu: boolean;
+  //public profile: Profile;
 
   constructor(
-    public angularService: AngularService,
+    /*public angularService: AngularService,
     public alertService: AlertService,
     public utilsService: UtilsService,
-    public userService: UserService,
+    public userService: UserService,*/
     public translateService: TranslateService) {
 
     // i18n configuration
@@ -30,7 +30,7 @@ export class AppComponent implements OnInit {
   ngOnInit() {
 
     // subscribe to the menu events
-    this.angularService.getMenu().subscribe(item => {
+    /*this.angularService.getMenu().subscribe(item => {
       this.menu = item;
       if (this.menu) {
         // the user is logged into the system
@@ -38,6 +38,6 @@ export class AppComponent implements OnInit {
           ((value: Profile) => this.profile = value),
           error => this.alertService.error(error));
       }
-    });
+    });*/
   }
 }
