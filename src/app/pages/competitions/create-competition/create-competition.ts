9362d31b0ac6bac2ff609d4d623f8743eb31bd2b
Maria del Mar <mariaderdalmata2@hotmail.com>
2018-04-18 17:25:17 +0200

    redistribution create-teams

--- src/app/pages/competitions/create-competition/create-competition.ts
+++ src/app/pages/competitions/create-competition/create-competition.ts
@@ -15,6 +15,7 @@ import { Response } from '@angular/http/src/static_response';
 })
 export class CreateCompetitionComponent implements OnInit {
 
+  finished = false;
   isLinear = true;
   competitionFormGroup: FormGroup;
   journeysFormGroup: FormGroup;
@@ -23,15 +24,6 @@ export class CreateCompetitionComponent implements OnInit {
   selectedParticipants: Array<number>;
   even: boolean;
 
-  types =  [
-    {value: 'Liga', viewValue: 'Liga'},
-    {value: 'Tenis', viewValue: 'Tenis'}
-  ];
-
-  modes =   [
-    {value: 'Individual', viewValue: 'Individual'},
-    {value: 'Equipos', viewValue: 'Equipos'}
-  ];
   groups = [];
 
   participant: any;
@@ -43,19 +35,15 @@ export class CreateCompetitionComponent implements OnInit {
   // Journeys
   journeys = new Array();
   newJourneys: Array<any>;
-  students = new Array<Student>();
 
   // Matches
-  match: Match;
-  match2: any;
-  previousStudents = new Array();
+  match: any;
   studentBefore: number;
   //
 
-  newCompetitionPost: Competition;
-  newCompetitionPost3: Competition;
-  newMatchesJourneys: Match;
-  relations: Response;
+  newCompetitionPost: Competition;  // b
+  count: number;
+  count2: number;
 
   constructor( public alertService: AlertService,
     public utilsService: UtilsService,
@@ -74,6 +62,7 @@ export class CreateCompetitionComponent implements OnInit {
   ngOnInit() {
 
     if (this.utilsService.role === Role.TEACHER) {
+
       // Defining 3 forms:
     this.competitionFormGroup = this._formBuilder.group({
       name: ['', Validators.required],
@@ -83,16 +72,16 @@ export class CreateCompetitionComponent implements OnInit {
       numJourneys: ['', Validators.required]
     });
 
+    this.participantsFormGroup = this._formBuilder.group({
+      participantId: ['']
+    });
+
     this.journeysFormGroup = this._formBuilder.group({
       journeys: this._formBuilder.array([
         this._formBuilder.group({
           date: ['']
         })
       ])
-    });
-
-    this.participantsFormGroup = this._formBuilder.group({
-      participantId: ['']
     });
 
     this.informationFormGroup = this._formBuilder.group({
@@ -115,43 +104,23 @@ export class CreateCompetitionComponent implements OnInit {
 
 
   // FIRST FORM: COMPETITION
-  onSubmit(value: Competition) {
-
+  competitionStep(value: Competition) {
     this.loadingService.show();
     this.newCompetition = value;
     this.newCompetition.teacherId = this.utilsService.currentUser.userId;
+    this.getParticipants(); // getting participants for the next step
+  }
 
-    // Add the journeys to the next step
-    for (let _n = 0; _n < this.newCompetition.numJourneys - 1; _n++) {
-      let journeys = <FormArray>this.journeysFormGroup.get('journeys');
-      journeys.push(this._formBuilder.group({
-        date: ['']
-      }));
-    }
-
-     // POST COMPETITION
-
-    this.competitionService.postCompetition(this.newCompetition)
-    .subscribe( (competition => {
-      this.newCompetitionPost = competition;
-      this.loadingService.hide();
-    }),
-    ((error: Response) => {
-      this.loadingService.hide();
-      this.alertService.show(error.toString());
-    }));
-
-    // GET STUDENTS SELECTED GROUP ( INDIVIDUAL MODE )
+  getParticipants(): void {
 
     if ( this.newCompetition.mode === 'Individual' ) {
       this.groupService.getMyGroupStudents(this.newCompetition.groupId).subscribe(
       ( (students: Array<Student>) => {
-        this.students = students;
-         for (let _n = 0; _n < this.students.length; _n++) {
+         for (let _n = 0; _n < students.length; _n++) {
          this.participant = {
-          id: this.students[_n].id,
-          name:  this.students[_n].name,
-          surname:  this.students[_n].surname,
+          id: students[_n].id,
+          name:  students[_n].name,
+          surname: students[_n].surname,
           selected: false
           };
           this.participants.push(this.participant);
@@ -162,11 +131,7 @@ export class CreateCompetitionComponent implements OnInit {
         this.loadingService.hide();
         this.alertService.show(error.toString());
       }));
-      }
-
-      // GET TEAMS SELECTED GROUP ( TEAM MODE )
-
-      if ( this.newCompetition.mode === 'Equipos' ) {
+      } else {
         this.groupService.getGroupTeams(this.newCompetition.groupId).subscribe(
         ( (teams: Array<Team>) => {
           for (let _a = 0; _a < teams.length; _a++) {
@@ -178,35 +143,60 @@ export class CreateCompetitionComponent implements OnInit {
                 };
              this.participants.push(this.participant);
           }
-          // tslint:disable-next-line:no-console
-          console.log(this.participants);
           this.loadingService.hide();
         }),
         ((error: Response) => {
           this.loadingService.hide();
           this.alertService.show(error.toString());
         }));
-        }
+      }
   }
 
-
- // SELECTED PARTICIPANTS
-  onSubmitParticipants(list) {
+  participantStep(list) {
+    this.loadingService.show();
     this.selectedParticipants = list.selectedOptions.selected.map(item => item.value);
+    this.selectedParticipants.length % 2 === 0 ? this.even = true : this.even = false;
     // tslint:disable-next-line:no-console
-    console.log(this.selectedParticipants);
-    if (this.selectedParticipants.length % 2 === 0 ) {
-      this.even = true;
-     } else {
-      this.even = false;
+    console.log(this.even);
+
+    // Add the journeys to the next step
+    for (let _n = 0; _n < this.newCompetition.numJourneys - 1; _n++) {
+      let journeys = <FormArray>this.journeysFormGroup.get('journeys');
+      journeys.push(this._formBuilder.group({
+       date: ['']
+      }));
     }
+    this.loadingService.hide();
   }
 
   // SECOND FORM: JOURNEYS
-  onSubmitJourneys (value) {
-
+  journeyStep(value) {
     this.loadingService.show();
     this.newJourneys = value.journeys;
+    this.loadingService.hide();
+  }
+
+  // THIRD FORM: INFORMATION
+  informationStep(value: string) {
+    this.loadingService.show();
+    this.newInformation = value;
+    this.onSubmitCompetition();
+  }
+
+  onSubmitCompetition(): void {
+    this.newCompetition.information = this.newInformation;
+    this.competitionService.postCompetition(this.newCompetition)
+    .subscribe( (competition => {
+      this.newCompetitionPost = competition;
+      this.onSubmitJourneys();
+    }),
+    ((error: Response) => {
+      this.loadingService.hide();
+      this.alertService.show(error.toString());
+    }));
+  }
+
+  onSubmitJourneys(): void {
     // Adding to the journeys: number and competitionId
     for ( let _n = 0; _n < this.newCompetition.numJourneys; _n++) {
       this.newJourneys[_n].number = _n + 1;
@@ -214,108 +204,93 @@ export class CreateCompetitionComponent implements OnInit {
       if ( this.newJourneys[_n].date === '' ) {
         this.newJourneys[_n].date = null;
       }
-     }
-       // POST JOURNEYS
-       for (let _a = 0; _a < this.newJourneys.length; _a++) {
-       this.journeyService.postJourney(this.newJourneys[_a])
-       .subscribe( (journey => {
-         this.journeys.push(journey);
-         this.loadingService.hide();
-       }),
+    }
+    // POST JOURNEYS
+    for (let _a = 0; _a < this.newJourneys.length; _a++) {
+      this.journeyService.postJourney(this.newJourneys[_a])
+      .subscribe( (journey => {
+       this.journeys.push(journey);
+       if (this.journeys.length === this.newJourneys.length) {
+        this.onSubmitRelations();
+       }
+      }),
        ((error: Response) => {
-         this.loadingService.hide();
-         this.alertService.show(error.toString());
-       }));
-      }
-
-        // relations
-    if ( this.newCompetition.mode === 'Individual' ) {
-    for ( let _i = 0; _i < this.selectedParticipants.length; _i++ ) {
-      this.competitionService.relCompetitionStudent(this.newCompetitionPost.id, this.selectedParticipants[_i]).subscribe(
-        ( res => {
-          this.relations = res;
-          this.loadingService.hide();
-        }),
-        ((error: Response) => {
-          this.loadingService.hide();
-          this.alertService.show(error.toString());
-        }));
-    }
-  } else {
-    for ( let _i = 0; _i < this.selectedParticipants.length; _i++ ) {
-      this.teamService.relCompetitionTeam(this.newCompetitionPost.id, this.selectedParticipants[_i]).subscribe(
-        ( res => {
-          this.relations = res;
-          this.loadingService.hide();
-        }),
-        ((error: Response) => {
-          this.loadingService.hide();
-          this.alertService.show(error.toString());
-        }));
+        this.loadingService.hide();
+        this.alertService.show(error.toString());
+      }));
     }
   }
 
-  }
-
-  // THIRD FORM: INFORMATION
-  onSubmitInf(value: string) {
-
-    this.loadingService.show();
-    this.newInformation = value;
-    this.competitionService.putInformation(this.newInformation, this.newCompetitionPost.id)
-    .subscribe( (competition => {
-      this.newCompetitionPost3 = competition;
-      this.loadingService.hide();
-    }),
-    ((error: Response) => {
-      this.loadingService.hide();
-      this.alertService.show(error.toString());
-    }));
-
-    this.league();
+  onSubmitRelations(): void {
+    this.count = 0;
+        if ( this.newCompetition.mode === 'Individual' ) {
+          for ( let _i = 0; _i < this.selectedParticipants.length; _i++ ) {
+            this.competitionService.relCompetitionStudent(this.newCompetitionPost.id, this.selectedParticipants[_i]).subscribe(
+              ( res => {
+                this.count++;
+                if ( this.count === this.selectedParticipants.length ) { this.league(); }
+              }),
+              ((error: Response) => {
+                this.loadingService.hide();
+                this.alertService.show(error.toString());
+              }));
+          }
+        } else {
+          for ( let _i = 0; _i < this.selectedParticipants.length; _i++ ) {
+            this.teamService.relCompetitionTeam(this.newCompetitionPost.id, this.selectedParticipants[_i]).subscribe(
+              ( res => {
+                this.count++;
+                if ( this.count === this.selectedParticipants.length ) { this.league(); }
+              }),
+              ((error: Response) => {
+                this.loadingService.hide();
+                this.alertService.show(error.toString());
+              }));
+          }
+        }
   }
 
   league(): void {
-      // ordenar ascendentemente las jornadas por su number
-      this.journeys.sort(function (a, b) {
-        return (a.number - b.number);
-      });
-
-      if (this.even === false) {
-        this.selectedParticipants.push(0);
-      }
-         // tslint:disable-next-line:no-console
-         console.log(this.selectedParticipants);
-    // Matches
-    for (let _j = 0; _j < this.journeys.length; _j++) {
-      for (let _m = 0; _m < (this.selectedParticipants.length / 2); _m++) {
-        this.match2 = {
-          playerOne : +this.selectedParticipants[_m],
-          playerTwo : +this.selectedParticipants[this.selectedParticipants.length - 1 - _m],
-          journeyId : +this.journeys[_j].id
-        };
-        this.match = this.match2;
-           // tslint:disable-next-line:no-console
-           console.log(this.match);
-          // POST MATCHES
-          this.journeyService.postJourneyMatches(this.match)
-          .subscribe( (match => {
-            this.newMatchesJourneys = match;
-            this.loadingService.hide();
-          }),
-          ((error: Response) => {
-            this.loadingService.hide();
-            this.alertService.show(error.toString());
-          }));
-      }
-
-      // Cambiamos las posiciones de los participantes en el vector de selectedParticipants
-      this.studentBefore =  this.selectedParticipants[1];
-      for (let _s = 1; _s < (this.selectedParticipants.length - 1); _s++) {
-        this.selectedParticipants[_s] = this.selectedParticipants[_s + 1];
-      }
-      this.selectedParticipants[this.selectedParticipants.length - 1] = this.studentBefore;
+    // ordenar ascendentemente las jornadas por su number
+    this.journeys.sort(function (a, b) {
+      return (a.number - b.number);
+    });
+    // adding ghost participant
+    if (this.even === false) {
+      this.selectedParticipants.push(0);
     }
-  }
+
+  // Matches
+  this.count2 = 0;
+   for (let _j = 0; _j < this.journeys.length; _j++) {
+    for (let _m = 0; _m < (this.selectedParticipants.length / 2); _m++) {
+      this.match = {
+        playerOne : +this.selectedParticipants[_m],
+        playerTwo : +this.selectedParticipants[this.selectedParticipants.length - 1 - _m],
+        journeyId : +this.journeys[_j].id
+      };
+        // POST MATCHES
+        this.journeyService.postJourneyMatches(this.match)
+        .subscribe( (match => {
+          this.count2++;
+          if ( this.count2 === (this.journeys.length * (this.selectedParticipants.length / 2)) ) {
+            this.finished = true;
+            this.loadingService.hide();
+          }
+        }),
+        ((error: Response) => {
+          this.loadingService.hide();
+          this.alertService.show(error.toString());
+        }));
+    }
+
+    // Cambiamos las posiciones de los participantes en el vector de selectedParticipants
+    this.studentBefore =  this.selectedParticipants[1];
+    for (let _s = 1; _s < (this.selectedParticipants.length - 1); _s++) {
+      this.selectedParticipants[_s] = this.selectedParticipants[_s + 1];
+    }
+    this.selectedParticipants[this.selectedParticipants.length - 1] = this.studentBefore;
+   }
+}
 
 }
